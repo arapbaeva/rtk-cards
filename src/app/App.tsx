93351d05f8c36fbc1859e48cbb3caf6ba00b2b6a
arapbaeva <aiymvector@gmail.com>
2023-04-29 08:36:44 +0600

    deleted pages, added routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,6 @@
 import { useAppDispatch, useAppSelector } from "app/hooks";
 import { useEffect } from "react";
-import { Counter } from "features/counter/Counter";
 import { appActions } from "app/app.slice";
-import { SignUp } from "features/auth/sign-up/SignUp";
-import { SignIn } from "features/auth/sign-in/SignIn";
 
 function App() {
   const isLoading = useAppSelector((state) => state.app.isLoading);
@@ -16,14 +13,7 @@ function App() {
     }, 3000);
   }, []);
 
-  return (
-    <div className="App">
-      {isLoading && <h1>Loader...</h1>}
-      <SignUp />
-      <SignIn />
-      <Counter />
-    </div>
-  );
+  return <div className="App">{isLoading && <h1>Loader...</h1>}</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,41 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { store } from "app/store";
 import App from "app/App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { SignIn } from "features/auth/sign-in/SignIn";
+import { SignUp } from "features/auth/sign-up/SignUp";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/login",
+    element: <SignIn />,
+  },
+  {
+    path: "/register",
+    element: <SignUp />,
+  },
+  {
+    path: "/packs",
+    element: <h1>Packs</h1>,
+  },
+]);
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
